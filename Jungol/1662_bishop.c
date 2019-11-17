158c436a36bdb4b20db5b8f6f3739913274d10b0
정재원 <45384654+myjungjw@users.noreply.github.com>
2019-11-17 09:32:22 +0900

    Update 1662_bishop.c
    bitmask test
    7
    1 0 1 1 1 0 1
    0 1 1 1 1 1 1
    1 1 1 1 1 1 1
    1 0 1 1 1 1 1
    0 1 0 0 0 1 1
    0 1 1 1 1 1 1
    0 0 0 0 0 0 1

--- Jungol/1662_bishop.c
+++ Jungol/1662_bishop.c
@@ -2,13 +2,10 @@
 
 int map[10][10];
 int N;
-int odd[100][3];
-int even[100][3];
-int odd_max = 0;
-int even_max = 0;
-int odd_cnt = 0;
-int even_cnt = 0;
 
+int bishop[100][3];
+int max = 0;
+int cnt = 0;
 
 void input() {
 	freopen("bishop.txt", "r", stdin);
@@ -24,80 +21,55 @@ void define() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			if (map[i][j] == 0) continue;
-
-			if ((i + j) % 2 == 1) {
-				odd[odd_cnt][0] = i;
-				odd[odd_cnt][1] = j;
-				odd[odd_cnt][2] = 0;
-				odd_cnt++;
-			}
-			if ((i + j) % 2 == 0) {
-				even[even_cnt][0] = i;
-				even[even_cnt][1] = j;
-				even[even_cnt][2] = 0;
-				even_cnt++;
-			}
+				bishop[cnt][0] = i;
+				bishop[cnt][1] = j;
+				bishop[cnt][2] = 0;
+				cnt++;
 		}
 	}
 }
 
-int even_check(int even_dep) {
-	for (int t = 0; t < even_dep; t++) {
-		if (even[even_dep][0] + even[even_dep][1] == even[t][0] + even[t][1] || even[even_dep][0] - even[even_dep][1] == even[t][0] - even[t][1]) {
-			if (even[t][2] == 1) return 1;  // 못 놓는 자리.
+int check(int dep) {
+	for (int t = 0; t < dep; t++) {
+		if (bishop[dep][0] + bishop[dep][1] == bishop[t][0] + bishop[t][1] || bishop[dep][0] - bishop[dep][1] == bishop[t][0] - bishop[t][1]) {
+			if (bishop[t][2] == 1) return 1;  // 못 놓는 자리.
 		}
 	}
 	return 0;   // 놓을 수 있는 자리
 }
 
-int odd_check(int odd_dep) {
-	for (int t = 0; t < odd_dep; t++) {
-		if (odd[odd_dep][0] + odd[odd_dep][1] == odd[t][0] + odd[t][1] || odd[odd_dep][0] - odd[odd_dep][1] == odd[t][0] - odd[t][1]) {
-			if (odd[t][2] == 1) return 1;  // 못 놓는 자리.
-		}
-	}
-	return 0;   // 놓을 수 있는 자리
-}
+int dfs(int index, int dep) {
+	if (dep == cnt) { max = cnt; return max; }
+	if (cnt - index < max - dep) return max;
+	if (dep > 2 * N - 1) return max;
 
-int odd_dfs(int odd_index, int odd_dep) {
-	if (odd_dep == odd_cnt) { odd_max = odd_cnt; return odd_max; }
-	if (odd_cnt - odd_index < odd_max - odd_dep) return odd_max;
 
-	if (odd[odd_index][2] == 0 && odd_check(odd_index) == 0) {
-		odd[odd_index][2] = 1;
-		if (odd_max < odd_dep + 1) odd_max = odd_dep + 1;
-		if (odd_index == odd_cnt - 1) return odd_max;
-		odd_dfs(odd_index + 1, odd_dep + 1);
-		odd[odd_index][2] = 0;
+	if (bishop[index][2] == 0 && check(index) == 0)
+	{
+		bishop[index][2] = 1;
+		if (max < dep + 1) max = dep + 1;
+		if (index == cnt - 1) return max;
+		dfs(index + 1, dep + 1);
+		bishop[index][2] = 0;
 	}
 
-	if (odd_index == odd_cnt) return odd_max;
-	else odd_dfs(odd_index + 1, odd_dep);
-}
-
-int even_dfs(int even_index, int even_dep) {
-	if (even_dep == even_cnt) { even_max = even_cnt; return even_max; }
-	if (even_cnt - even_index < even_max - even_dep) return even_max;
-
-	if (even[even_index][2] == 0 && even_check(even_index) == 0) {
-		even[even_index][2] = 1;
-		if (even_max < even_dep + 1) even_max = even_dep + 1;
-		if (even_index == even_cnt - 1) return even_max;
-		even_dfs(even_index + 1, even_dep + 1);
-		even[even_index][2] = 0;
-	}
-
-	if (even_index == even_cnt) return even_max;
-	else even_dfs(even_index + 1, even_dep);
+	if (index == cnt - 1) return max;
+	else dfs(index + 1, dep);
 }
 
 int main()
 {
 	input();
 	define();
-    odd_dfs(0,0);
-	even_dfs(0, 0);
-	printf("%d %d", odd_max,even_max);
-
+	dfs(0, 0);
+	//output();
+	if (cnt == 0) {
+		printf("%d", cnt);
+	}
+	else {
+		printf("%d", max);
+	}
 	return 0;
 }
+
+
