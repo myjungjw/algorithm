d8425bf133622a2d9e09e3d76b25c85b281c44b0
정재원 <45384654+myjungjw@users.noreply.github.com>
2020-03-31 07:02:02 +0900

    Update 1822_Paramecium.c

--- Jungol/1822_Paramecium.c
+++ Jungol/1822_Paramecium.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
-int a, b, d, N, temp;
+int a, b, d, N, birth;
 int bug[10001];
-int birth() {
-	int sum=0;
-	for (int i = (a-1); i < (b-1); i++) {
+
+int sum(int s, int e){
+	int sum = 0 ;
+	for ( int i = s ;i < e ; i++){
+		if ( bug[i] == 0 ) continue;
 		sum += bug[i];
 	}
 	return sum%1000;
 }
 
-int result() {
-	int sum=0;
-	for (int i = 0; i < d; i++) {
-		sum += bug[i];
-	}
-	return sum%1000;
-}
 int main() {
+	freopen("in.txt", "r", stdin);
 	scanf("%d %d %d %d", &a, &b, &d, &N);
-	for (int i = 0; i <= N; i++) {
-		if (i == 0) {	
-			bug[0] = 1;
-			continue;
+	bug[0] = 1;
+	for (int i = 1; i <= N; i++) {
+		birth = sum(a-1, b-1);
+		for ( int j = (d - 1) ; j > 0 ; j--) {
+			bug[j] = bug[j - 1];
 		}
-		else {
-			temp = birth();
-			for (int j = (d - 1) ; j > 0 ; j--) {
-				bug[j] = bug[j - 1];
-			}
-			bug[0] = temp;
-		}
+		bug[0] = birth;
 	}
-	printf("%d", result());
+	printf("%d", sum(0, d));
 	return 0;
 }
