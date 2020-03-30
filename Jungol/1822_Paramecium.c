f5a463166a295a4d2430ff6404d50c201d7b116f
정재원 <45384654+myjungjw@users.noreply.github.com>
2020-03-31 07:04:49 +0900

    Update 1822_Paramecium.c
    basic

--- Jungol/1822_Paramecium.c
+++ Jungol/1822_Paramecium.c
@@ -4,10 +4,7 @@ int bug[10001];
 
 int sum(int s, int e){
 	int sum = 0 ;
-	for ( int i = s ;i < e ; i++){
-		if ( bug[i] == 0 ) continue;
-		sum += bug[i];
-	}
+	for ( int i = s ;i < e ; i++) sum += bug[i];
 	return sum%1000;
 }
 
@@ -17,9 +14,7 @@ int main() {
 	bug[0] = 1;
 	for (int i = 1; i <= N; i++) {
 		birth = sum(a-1, b-1);
-		for ( int j = (d - 1) ; j > 0 ; j--) {
-			bug[j] = bug[j - 1];
-		}
+		for ( int j = (d - 1) ; j > 0 ; j--) bug[j] = bug[j - 1];
 		bug[0] = birth;
 	}
 	printf("%d", sum(0, d));
