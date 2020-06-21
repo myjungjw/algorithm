d3f9fd1ccb057a83a96bb16e9aad8002fa88bb57
정재원 <45384654+myjungjw@users.noreply.github.com>
2020-06-22 02:54:18 +0900

    Update Program.cs
    Player

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,19 +8,29 @@ namespace Algorithm
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.Initialize(25);
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
 
             Console.CursorVisible = false;
             
             const int WAIT_TICK = 1000 / 30;
+
             int lastTick = 0;
             while (true)
             {
                 #region 프레임 관리
                 int currentTick = System.Environment.TickCount;
-                if (currentTick - lastTick < WAIT_TICK) continue;
+                int deltaTick = currentTick - lastTick;
+                if (deltaTick < WAIT_TICK) continue;
                 lastTick = currentTick;
                 #endregion
+                // 입력
+
+                // 로직
+                player.Update(deltaTick);
+
+                // 랜더링
                 Console.SetCursorPosition(0, 0);
                 board.Render();
             }
