2d891892d19b000da7e9518f384b70af2865f1d2
정재원 <45384654+myjungjw@users.noreply.github.com>
2020-06-22 09:35:33 +0900

    Update Player.cs
    Right Hand Rule

--- CSharp/Player.cs
+++ CSharp/Player.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Dynamic;
 using System.Text;
 
 namespace Algorithm
 {
+    class Pos
+    {
+        public Pos(int y, int x) { Y = y; X = x; }
+        public int Y;
+        public int X;
+    }
     class Player
     {
         public int PosY { get; private set; }
@@ -12,44 +19,70 @@ namespace Algorithm
         Random _rand = new Random();
         Board _board;
 
+        enum Dir
+        {
+            Up = 0,  
+            Left = 1,  
+            Down = 2, 
+            Right = 3 
+        }
+        int _dir = (int)Dir.Up;
+        List<Pos> _points = new List<Pos>();
+
         public void Initialize(int posY, int posX, Board board)
         {
             PosY = posY;
             PosX = posX;
-
             _board = board;
+
+            // 현재 방향이 ULDR0123 일 경우 front와 right 방향
+            int[] frontY = new int[] { -1, 0, 1, 0 };
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+            int[] rightY = new int[] { 0, -1, 0, 1 };
+            int[] rightX = new int[] { 1, 0, -1, 0 };
+
+            _points.Add(new Pos(PosY, PosX));
+            while ( PosY != board.DestY || PosX != board.DestX)
+            {
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)   // 우측회전 가능
+                {
+                    // 오른쪽 방향으로 회전
+                    _dir = (int)((_dir + 3) % 4);
+                    // 일보전진
+                    PosY += frontY[_dir];
+                    PosX += frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty ) // 직진 가능
+                {
+                    // 일보전진
+                    PosY += frontY[_dir];
+                    PosX += frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else
+                {
+                    // 왼쪽 방향으로 회전
+                    _dir = (int)((_dir + 5) % 4);
+                }
+            }
+
         }
 
         const int MOVE_TICK = 40;
         int _sumTick = 0;
+        int _lastIndex = 0;
         public void Update(int deltaTick)
         {
+            if (_lastIndex >= _points.Count) return;
             _sumTick += deltaTick;
             if (_sumTick >= MOVE_TICK)
             {
                 _sumTick = 0;
-                // 0.1"마다 실행될 코드 
-                int randValue = _rand.Next(0, 4);
-                switch (randValue)
-                {
-                    case 0: // 상
-                        if (PosY - 1 >= 0 && _board.Tile[PosY - 1, PosX] == TileType.Empty)
-                            PosY--;
-                        break;
-                    case 1: // 하
-                        if (PosY + 1 < _board.Size && _board.Tile[PosY + 1, PosX] == TileType.Empty)
-                            PosY++;
-                        break;
-                    case 2: // 좌
-                        if (PosX - 1 >= 0 && _board.Tile[PosY, PosX - 1] == TileType.Empty)
-                            PosX--;
-                        break;
-                    case 3: // 우
-                        if (PosX + 1 < _board.Size && _board.Tile[PosY, PosX + 1] == TileType.Empty)
-                            PosX++;
-                        break;
 
-                }
+                PosY = _points[_lastIndex].Y;
+                PosX = _points[_lastIndex].X;
+                _lastIndex++;
             }
         }
     }
